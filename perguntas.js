0fa5286bc55d83c66e8063f04219f49693f12ec3
vovozona4 <140420967+vovozona4@users.noreply.github.com>
2024-11-11 16:23:37 -0300

    c

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,24 @@
 
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Geografia',
+    'Qual é a capital da Irlanda? ',
+    'Dublin'
+)
+
+criaCartao(
+    ' ciência',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o nome do rio que atravessa Londres?',
+    ' Rio Tâmisa'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'matemática',
+    ' Quanto é 4 x 6?',
+    '24'
 )
